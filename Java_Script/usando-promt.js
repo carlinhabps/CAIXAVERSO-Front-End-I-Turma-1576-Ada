caff1b0533bb6a28a5576a9a11b15ad637276de9
Cala Beatriz <carlinhabps@hotmail.com>
2026-01-05 21:21:25 -0300

    exercícios da aula 1

---
+++ Java_Script/usando-promt.js
@@ -0,0 +1,34 @@
+// //  * Passo 1 - Entrada de dados
+
+// let nome = prompt("Digite seu nome:");
+
+// let entrada1 = prompt("Digite a primeira nota:");
+// let entrada2 = prompt("Digite a segunda nota:");
+
+// // * Passo 2 - Conversão de string para number
+
+// let n1 = Number(entrada1);
+// let n2 = Number(entrada2);
+
+// // * Passo 3 - Cálculo
+
+// const mediaFinal = (n1 + n2) / 2;
+
+// // * Passo 4 - Saída visual
+
+// // alert("A média do aluno é: " + mediaFinal);
+// alert(`Olá ${nome}, sua média é ${mediaFinal}`);
+// console.log("A média do aluno é:", mediaFinal);
+// console.log(`Olá ${nome}, sua média é ${mediaFinal}`);
+
+// ! EXERCÍCIO
+
+let nomeAluno = prompt("Nome do Aluno");
+
+let noota1 = prompt("Digite a nota da primeira prova:");
+
+let noota2 = prompt("Digite a nota da segunda prova:");
+
+const notaFinal = (noota1 * 4 + noota2 * 6) / 10;
+
+alert(`Olá ${nomeAluno}, sua média do ano é ${notaFinal}`);
