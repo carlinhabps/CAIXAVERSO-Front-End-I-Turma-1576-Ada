b89ad8742507818d309a57b27ff4703078425e00
Cala Beatriz <carlinhabps@hotmail.com>
2026-01-25 15:29:23 -0300

    Remove Java_Script folder from HTML_CSS branch

--- Java_Script/Aula-1/script.js
+++
@@ -1,8 +0,0 @@
-console.log("Olá mundo!");
-// alert("Isso é um alerta!");
-
-let nome = "Carla";
-
-console.log("Meu nome é: " + nome);
-console.log("Meu nome é:", nome);
-console.log(`Meu nome é: ${nome}`);
