25724b0485b68c52a654760768760e04755c4488
Cala Beatriz <carlinhabps@hotmail.com>
2026-01-25 23:47:21 -0300

    uso do try/catch, porém, está dando erro quando tento add um crédito mesmo depois do erro

--- Java_Script/Projeto-Final/script.js
+++ Java_Script/Projeto-Final/script.js
@@ -32,9 +32,13 @@ function atualizarSaldo() {
     } else {
       debitos += transacoes[i].valor;
     }
-
-    usuario.saldo = creditos - debitos;
   }
+
+  if (debitos > creditos) {
+    throw new Error("Saldo insuficiente para realizar a transação");
+  }
+
+  usuario.saldo = creditos - debitos;
 
   console.log(usuario.saldo);
 
@@ -77,42 +81,52 @@ function renderizarExtrato() {
 }
 
 function novaTransacao(botao) {
-  let valorInformado = prompt("Informe o valor da transação");
+  try {
+    let valorInformado = prompt("Informe o valor da transação");
 
-  valorInformado = Number(valorInformado.replace(",", "."));
+    valorInformado = Number(valorInformado.replace(",", "."));
 
-  if (isNaN(valorInformado)) {
-    alert("Por favor, informe um valor numérico válido.");
-    return novaTransacao(botao);
-  } else if (valorInformado < 0) {
-    alert("Por favor, informe o valor SEM o sinal de menos!");
-    return novaTransacao(botao);
+    if (isNaN(valorInformado)) {
+      alert("Por favor, informe um valor numérico válido.");
+      return novaTransacao(botao);
+    } else if (valorInformado < 0) {
+      alert("Por favor, informe o valor SEM o sinal de menos!");
+      return novaTransacao(botao);
+    }
+
+    let descricaoInfromada = prompt(
+      "Por favor, informe a descrição da transação:",
+    );
+    if (!descricaoInfromada) {
+      descricaoInfromada = botao;
+    }
+
+    let tipoMovimentacao;
+    if (botao === "deposito") {
+      tipoMovimentacao = "C";
+    } else if (botao === "saque") {
+      tipoMovimentacao = "D";
+    } else if (botao === "pagamento") {
+      tipoMovimentacao = "D";
+    }
+
+    transacoes.push({
+      valor: valorInformado,
+      descricao: descricaoInfromada,
+      transacao: tipoMovimentacao,
+      data: new Date(),
+      id: Date.now(),
+    });
+
+    atualizarSaldo();
+    renderizarExtrato();
+  } catch (erro) {
+    console.error("Erro capturado:", erro);
+
+    html("#lista-transacoes").innerHTML += `
+    <tr class="erro">
+      <td colspan = 5 >${erro.message}</td>
+    </tr>
+  `;
   }
-
-  let descricaoInfromada = prompt(
-    "Por favor, informe a descrição da transação:",
-  );
-  if (!descricaoInfromada) {
-    descricaoInfromada = botao;
-  }
-
-  let tipoMovimentacao;
-  if (botao === "deposito") {
-    tipoMovimentacao = "C";
-  } else if (botao === "saque") {
-    tipoMovimentacao = "D";
-  } else if (botao === "pagamento") {
-    tipoMovimentacao = "D";
-  }
-
-  transacoes.push({
-    valor: valorInformado,
-    descricao: descricaoInfromada,
-    transacao: tipoMovimentacao,
-    data: new Date(),
-    id: Date.now(),
-  });
-
-  atualizarSaldo();
-  renderizarExtrato();
 }
